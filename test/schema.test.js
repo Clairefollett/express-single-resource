e199703824af1412088e2a9495a981f7b0d07547
Clarefollett <chlaw101@gmail.com>
2017-04-07 13:37:50 -0700

    schema tests done

--- test/schema.test.js
+++ test/schema.test.js
@@ -20,17 +20,52 @@ describe('schnoodle schema', () => {
         .then(
             () => { throw new Error('validation should fail'); },
             res => {
-                const errorMsg = 'Path `name` is required.'
-                assert.equal(res.errors.name.message, errorMsg)  
+                const errorMsg = 'Path `name` is required.';
+                assert.equal(res.errors.name.message, errorMsg); 
             }
-        )
+        );
     });
 
     it('requires the type', () => {
-        
+        return new Schnoodle({
+            name: 'schnoodle',
+            age: 50
+        }).validate()
+        .then(
+            () => { throw new Error('validation should fail'); },
+            res => {
+                const errorMsg = 'Path `type` is required.';
+                assert.equal(res.errors.type.message, errorMsg);  
+            }
+        );    
     });
 
     it('requires the age', () => {
-        
+        return new Schnoodle({
+            name: 'breezy',
+            type: 'doggy'
+        }).validate()
+        .then(
+            () => { throw new Error('validation should fail'); },
+            res => {
+                const errorMsg = 'Path `age` is required.';
+                assert.equal(res.errors.age.message, errorMsg) ; 
+            }
+        );      
     });
-})
+
+    it('fails validation if not the right type', () => {
+        return new Schnoodle({
+            name: 'testing123',
+            type: 'doggy',
+            age: 'hello'
+        }).validate()
+        .then(
+            () => { throw new Error('validation should fail'); },
+            res => {
+                const errorMsg = 'Cast to Number failed for value "hello" at path "age"';
+                assert.equal(res.errors.age.message, errorMsg);  
+            }
+        );
+    });
+});
